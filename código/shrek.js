c4366751316d60eeb65dce51a871c3d197647ed2
Daniel Pessoa Schmockel <danielpschmockel@hotmail.com>
2025-02-04 13:29:12 -0300

    nao tava funfando, agora ta

--- código/shrek.js
+++ código/shrek.js
@@ -1,32 +1,38 @@
-function enviarScript(scriptText){
-    const lines = scriptText.split("\n"); 
-
-    let i = 0;       
-
-    setInterval(() => { 
-        if(i >= lines.length) return
-
-        if(lines[i].trim() != ''){ 
-            console.log(lines[i]); 
-    
-            window.InputEvent = window.Event || window.InputEvent;
-    
-            const event = new InputEvent('input', { bubbles: true });
-    
-            const textbox = document.querySelector('#main div._13NKt');
-    
-            textbox.textContent = lines[i];
-    
-            textbox.dispatchEvent(event);
-    
-            document.querySelector("#main span[data-testid=send]").click();                   
-
-        }
-        i++;
-    }, 250)
+async function enviarScript(scriptText){
+	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+	main = document.querySelector("#main"),
+	textarea = main.querySelector(`div[contenteditable="true"]`)
+	
+	if(!textarea) throw new Error("Não há uma conversa aberta")
+	
+	for(const line of lines){
+		console.log(line)
+	
+		textarea.focus();
+		document.execCommand('insertText', false, line);
+		textarea.dispatchEvent(new Event('change', {bubbles: true}));
+	
+		setTimeout(() => {
+			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+		}, 100);
+		
+		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+	}
+	
+	return lines.length;
 }
 
-enviarScript(`SHREK
+enviarScript(`
+SHREK
+
+escrito por
+
+William Steig & Ted Elliott
+
+
+
+
+SHREK
 Era uma vez um adorável
 Princesa. Mas ela tinha um encantamento
 sobre ela de uma espécie terrível que poderia
@@ -2766,4 +2772,6 @@ BURRO
 (quando ele termina de cantar e nós desaparecemos para
 preto) Oh, isso é engraçado. Oh. Oh. Não posso
 respirar. Eu não consigo respirar.
-FIM`);
+FIM
+
+`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
